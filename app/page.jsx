f0f2db53886b325c65e51dc6b37b2b93d1fbe6c2
DESKTOP-U75F7MN\Benjamin <bpolin@consolidate.eu>
2023-05-28 15:07:37 +0200

    add first designs and texts

--- app/page.jsx
+++ app/page.jsx
@@ -1,5 +1,18 @@
 const Home = () => {
-  return <div>Home</div>;
+  return (
+    <section className="w-full flex-center flex-col">
+      <h1 className="head_text text-center">
+        Discover & Share <br className="max-md:hidden" />
+        <span className="orange_gradient text-center">AI-Powered Promts</span>
+      </h1>
+      <p className="desc text-center">
+        Next-Prompts is an open-source AI promting tool for the modern world to
+        discover, create and share creative promts
+      </p>
+
+      {/* Feed */}
+    </section>
+  );
 };
 
 export default Home;
