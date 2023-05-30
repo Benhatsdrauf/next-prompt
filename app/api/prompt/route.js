4a8e8a576c729bcb534a421409d72776c73e26d5
DESKTOP-U75F7MN\Benjamin <bpolin@consolidate.eu>
2023-05-30 12:00:31 +0200

    fix ?

--- app/api/prompt/route.js
+++ app/api/prompt/route.js
@@ -8,6 +8,9 @@ export const GET = async () => {
     const prompts = await Prompt.find().populate("creator");
     return new Response(JSON.stringify(prompts), {
       status: 200,
+      headers: {
+        "Cache-Control": "no-store",
+      },
     });
   } catch (error) {
     return new Response("Failed to fetch all prompts", { status: 500 });
